5addb8ea26d03b8ac2f4e0d5cfc1aaa99ea22654
adriana <adrianamolello@gmail.com>
2016-02-05 20:22:57 +0000

    Added destroy button

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,10 +20,16 @@ class PlacesController < ApplicationController
 		@place = Place.find(params[:id])
 	end
 	def update
+  		@place = Place.find(params[:id])
+  		@place.update_attributes(place_params)
+  		redirect_to root_path
+	end
+	def destroy
 		@place = Place.find(params[:id])
-		@place.update_attributes(place_params)
+		@place.destroy
 		redirect_to root_path
 	end
+
 
 	private
 
