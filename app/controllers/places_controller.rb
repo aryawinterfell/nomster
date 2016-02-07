8e82db527fe9bfa29d7e079435bf956071c7b720
adriana <adrianamolello@gmail.com>
2016-02-07 20:07:33 +0000

    error messages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :new, status: :unprocessable_entity
+		end
 	end
 	def show
 		@place = Place.find(params[:id])
@@ -30,7 +34,11 @@ class PlacesController < ApplicationController
   		end
 
   		@place.update_attributes(place_params)
+  		if @place.valid?
   		redirect_to root_path
+  	else
+  		render :edit, status: :unprocessable_entity
+  	end
 	end
 	def destroy
 		@place = Place.find(params[:id])
