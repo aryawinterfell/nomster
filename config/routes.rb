6ceb97b4fc9356c5c586b8516cb6d76dae40ba53
adriana <adrianamolello@gmail.com>
2016-02-11 19:35:49 +0000

    Added photo model

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Nomster::Application.routes.draw do
   devise_for :users
   root 'places#index'
   resources :places do
-    resources :comments, only: :create
+  resources :comments, only: :create
   end
 
 
